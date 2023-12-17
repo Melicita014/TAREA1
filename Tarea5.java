2ae320d0111d6cad6983f7d9ea6f5d2363af5f17
Melicita014 <145325113+Melicita014@users.noreply.github.com>
2023-12-17 13:52:33 +0100

    Add files via upload

---
+++ Tarea5.java
@@ -0,0 +1,25 @@
+public class Tarea5 {
+	static int factorial (int x) {
+		
+	if (x==0||x==1)
+		return 1 ;
+	else
+		return x * factorial(x-1);
+	}
+	public static void main(String[] args) {
+		int[] tabla = new int[10];
+	
+		
+	//Rellenar la tabla con factorial
+		for (int i = 0; i <= tabla.length -1 ; i++)
+			tabla[i]= factorial(i);
+		
+	//Mostrar la tabla en pantalla
+		for (int i = 0; i <= tabla.length -1 ; i++)
+		System.out.println(i + ": " +tabla[i] );
+		
+	
+		
+	}
+}
+
