86b6f7a8eeab2305b4f987462e59a03ace1e631a
Melicita014 <145325113+Melicita014@users.noreply.github.com>
2023-12-18 18:29:32 +0100

    Tara9.java

--- Tara9.java
+++ Tara9.java
@@ -1,22 +1,32 @@
 //En un array de 8 posiciones, cargar valores aleatorios 
 //de 0 a 39(ambos límites incluidos), pero sin que se repitan.
 public class Tara9 {
-
-	public static void main(String[] args) {
-		int[] tabla = new int[8]; // Creo una tabla vacia de 8 espacios y los lleno con numeros randoms
-					for(int i = 1;i< 7;i++) {
-						tabla[i]=(int)Math.floor(Math.random()*39 ) ;
-					}
 	
-					
-			for (int i = 0; i < tabla.length; i++) {{ // Localiza si hay numeros repetidos
-				for (int j = 0 ; j < tabla.length; j++)
-					if(tabla[i] == tabla[j])		
-						tabla[i]=(int)Math.floor(Math.random()*39 );// Le da otro valor
-			}			
-				System.out.println(tabla[i]);
+	// Funcion para comprobar si esta repetido o no
 	
-			}
-
+	static boolean seRepite (int t[], int numero, int cantidad) {
+		for (int i=0 ; i<cantidad;i++)
+			if (t[i]== numero)
+				return true ;
+				
+			return false ;
 	}
-}
+	
+		public static void main(String[] args) {
+			
+		int[] tabla = new int[8];
+		//Calcular 8 posiciones diferentes
+				for(int i = 1;i< tabla.length;i++) {
+							int aleatorio ;
+							aleatorio =(int)Math.floor(Math.random()*39 );
+							
+				while (seRepite(tabla, aleatorio, i))
+								aleatorio =(int)Math.floor(Math.random()*39 );
+								tabla[i]= aleatorio;
+				}
+		// Visualizar tabla
+				for(int i = 0;i< tabla.length;i++)
+							System.out.println(tabla[i]);
+		}
+	}
+
